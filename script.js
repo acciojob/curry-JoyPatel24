badeb56d635af5ba32cc47b51122d0db578a6212
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2024-02-07 08:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function curry(callback) {
-  function curriedFn(...args){
-	  if(args.length >= callback.length){
-		  return callback(...args);
-	  }else{
-		  return (...moreArgs) => curriedFn(...args, ...moreArgs);
-	  }
-  }
-	return curriedFn;
-}
+            function curriedFn(...args) {
+                if (args.length >= callback.length) {
+                    return callback(...args);
+                } else {
+                    return (...moreArgs) => curriedFn(...args, ...moreArgs);
+                }
+            }
+            return curriedFn;
+        }
 
-module.export=curry
+        // Sample usage
+        const sum = (...numbers) => numbers.reduce((total, number) => total + number, 0);
+        const curriedSum = curry(sum);
+
+        document.getElementById('btn').addEventListener('click', function() {
+            const result = curriedSum(1)(2)(3)();
+            document.getElementById('output').textContent = `Result: ${result}`;
+        });
